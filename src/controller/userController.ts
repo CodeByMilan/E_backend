14812d2a499bc12d6e555a1eb91d221b853c464c
Milan Acharya <milanacharya1614@gmail.com>
2024-10-30 13:06:26 +0545

    added hashing

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,5 +1,6 @@
 import { Request,Response } from "express";
 import User from "../database/models/User";
+import bcrypt from  "bcrypt";
 
 class AuthController {
    public static async registerUser(req:Request,res:Response):Promise<void>
@@ -14,7 +15,9 @@ class AuthController {
             await User.create({
                 username,
                 email,
-                password
+                //8:salt value means deciding the strength of the password 
+                //putting the more salt value results in more time to hash the password due to which it will take more time so for better user experience its recommended to use middle value  of salt
+                password:bcrypt.hashSync(password,10)
             })
             res.status(200).json({
                 message:"User created successfully"

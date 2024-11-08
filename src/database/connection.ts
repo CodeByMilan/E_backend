90e09eb9373be9d247248c1a0e8f63ff3cb856b5
Milan Acharya <milanacharya1614@gmail.com>
2024-11-08 07:29:57 +0545

    new tables added

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,6 +3,9 @@ import User from "./models/User";
 import Product from "./models/Product";
 import Category from "./models/Category";
 import Cart from "./models/Cart";
+import Order from "./models/Order";
+import OrderDetail from "./models/OrderDetails";
+import Payment from "./models/Payment";
 const sequelize = new Sequelize({
   database:process.env.DB_NAME,
   dialect: 'mysql',
@@ -42,6 +45,18 @@ sequelize
     //relation cart-user
     User.hasMany(Cart,{foreignKey:'userId'})
     Cart.belongsTo(User,{foreignKey:'userId'})
+
+    //order-orderdetails relation
+    Order.hasMany(OrderDetail,{foreignKey:'orderId'})
+    OrderDetail.belongsTo(Order,{foreignKey:'orderId'})
+
+    //product - orderdetail ralationship
+    Product.hasMany(OrderDetail,{foreignKey:'productId'})
+    OrderDetail.belongsTo(Product,{foreignKey:'productId'})
+
+    //payment - order relationship
+    Order.belongsTo(Payment,{foreignKey:'paymentId'})
+    Payment.hasOne(Order,{foreignKey:'paymentId'})
 
 
 
---
+++ src/database/models/Payment.ts
@@ -0,0 +1,32 @@
+import { Table, Column, Model, DataType } from "sequelize-typescript";
+
+@Table({
+  tableName: "payments",
+  modelName: "Payment",
+  timestamps: true,
+})
+class Payment extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  declare id: string;
+ 
+  @Column({
+    type: DataType.ENUM('COD','E-sewa','Khalti'),
+    allowNull:false 
+  })
+  declare paymentMethod: string;
+  @Column({
+    type: DataType.ENUM('paid','unpaid'),
+    defaultValue :'unpaid'
+  })
+  declare paymentStatus: string;
+  @Column({
+    type: DataType.STRING,
+  })
+  declare pidx:string
+
+}
+export default Payment;

3a0ba76ea5bfbbca473bae45fee8ba1b8e68b396
Milan Acharya <milanacharya1614@gmail.com>
2024-12-17 22:48:48 +0545

    updated

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,8 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "http://localhost:5174"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization", "Origin", "X-Requested-With"],
   },
 });
 
--- src/middleware/multerConfig.ts
+++ src/middleware/multerConfig.ts
@@ -3,7 +3,7 @@ import {Request} from 'express'
 
 const storage = multer.diskStorage({
     destination : function(req:Request,file:Express.Multer.File,cb:any){
-        const allowedFileTypes = ['image/jpg','image/png','image/jpeg']
+        const allowedFileTypes = ['image/jpg','image/png','image/jpeg','image/avif','image/webp']
         if(!allowedFileTypes.includes(file.mimetype)){
             cb(new Error("This filetype is not accepted"))
             return
